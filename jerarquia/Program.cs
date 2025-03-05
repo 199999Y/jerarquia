352dc3517deb5971a3bd0ac52ae37113d0be167c
199999Y <eidanjunior.ariasp@gmail.com>
2025-03-05 14:36:47 -0500

    Add files via upload

---
+++ jerarquia/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Vehiculo
+{
+    public string Marca { get; set; }
+    public int Año { get; set; }
+
+    public void MostrarInfo()
+    {
+        Console.WriteLine($"Vehículo: {Marca}, Año: {Año}");
+    }
+}
+
+class Auto : Vehiculo
+{
+    public int Puertas { get; set; }
+
+    public void MostrarDetalles()
+    {
+        Console.WriteLine($"Auto: {Marca}, Año: {Año}, Puertas: {Puertas}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Auto miAuto = new Auto { Marca = "Toyota", Año = 2022, Puertas = 4 };
+        miAuto.MostrarDetalles();
+    }
+}
+
